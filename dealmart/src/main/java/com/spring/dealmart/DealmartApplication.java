e365fd3ee63e8fbcb0a9b7ddff979a74b5591e55
pankajdeep <pankajdeepsahota@gmail.com>
2024-12-11 15:12:20 -0500

    Creating project and setting up the files.

---
+++ dealmart/src/main/java/com/spring/dealmart/DealmartApplication.java
@@ -0,0 +1,13 @@
+package com.spring.dealmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealmartApplication.class, args);
+	}
+
+}
---
+++ dealmart/src/test/java/com/spring/dealmart/DealmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.dealmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
